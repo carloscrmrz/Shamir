83557c4af3fb7386bee618b450ce3d23fd818cb1
Carlos Cabrera <ccabrera@bluepeople.com>
2022-06-13 17:12:38 -0500

    Created Math logic

--- shamirmath/polynomials.py
+++ shamirmath/polynomials.py
@@ -1,30 +1,33 @@
 from . import mod_math
 
-def eval_polynomial_at(x, polynomial, prime):
-  acc = 0
-  for coefficient in reversed(polynomial):
-    acc *= x
-    acc += coefficient
-    acc %= prime
-  return acc
 
-def product_inputs(vals):
-  acc = 1
-  for v in vals:
-    acc *= v
-  return acc
+def eval_polynomial_at(x, polynomial, prime):
+    acc = 0
+    for coeff in reversed(polynomial):
+        acc *= x
+        acc += coeff
+        acc %= prime
+    return acc
+
+
+def product_lagrange(vals):
+    accum = 1
+    for v in vals:
+        accum *= v
+    return accum
+
 
 def lagrange_interpolation(x, x_s, y_s, p):
-  k = len(x_s)
+    k = len(x_s)
 
-  nums = []
-  dens = []
-  for i in range(k):
-    others = list(x_s)
-    current = others.pop(i)
-    nums.append(product_inputs(x - a for a in others))
-    dens.append(product_inputs(current - a for a in others))
-  den = product_inputs(dens)
-  num = sum([mod_math.modular_division(nums[i] * den * y_s[i] % p, dens[i], p) for i in range(k)])
-
-  return (mod_math.modular_division(num,den,p) + p) % p
+    nums = []
+    dens = []
+    for i in range(k):
+        others = list(x_s)
+        cur = others.pop(i)
+        nums.append(product_lagrange((x - o for o in others)))
+        dens.append(product_lagrange((cur - o for o in others)))
+    den = product_lagrange(dens)
+    num = sum([mod_math.modular_division(nums[i] * den * y_s[i] %
+              p, dens[i], p) for i in range(k)])
+    return (mod_math.modular_division(num, den, p) + p) % p
